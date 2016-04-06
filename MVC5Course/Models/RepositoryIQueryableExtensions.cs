9c850b51cc20881c1fbd9d38cdda96ac67005966
Will <doggy.huang@gmail.com>
2016-04-06 11:25:25 +0800

    關於 IQueryable<T>.Include() 的用法語說明 https://msdn.microsoft.com/en-us/library/system.data.entity.queryableextensions.include.aspx

--- MVC5Course/Models/RepositoryIQueryableExtensions.cs
+++ MVC5Course/Models/RepositoryIQueryableExtensions.cs
@@ -1,18 +1,24 @@
+using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 
 namespace MVC5Course.Models
 {
 	public static class RepositoryIQueryableExtensions
 	{
-		public static IQueryable<T> Include<T>
-			(this IQueryable<T> source, string path)
-		{
-			var objectQuery = source as ObjectQuery<T>;
-			if (objectQuery != null)
-			{
-				return objectQuery.Include(path);
+        public static IQueryable<T> Include<T>
+            (this IQueryable<T> source, string path) where T : class
+        {
+            // Ref: https://msdn.microsoft.com/en-us/library/system.data.entity.queryableextensions.include.aspx
+            // See "Remarks"!
+
+            if (source is ObjectQuery<T> || source is DbSet<T> ||
+                source is DbQuery || source is DbSet)
+            {
+				return source.Include(path);
 			}
+
 			return source;
 		}
 	}
