4f195c4488ac1d2b8c4c8d3ba12b468ec2a37ec1
Will <doggy.huang@gmail.com>
2016-04-18 14:42:24 +0800

    加入 ASP.NET Web API 專案

---
+++ WebApi1/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static List<Product> products = new List<Product>()
+           {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+           };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [ValidateModel]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            products.Add(product);
+
+            return Created(
+                Url.Route("DefaultApi", new { id = product.Id }),
+                product);
+        }
+
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApi1/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
